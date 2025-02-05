db899aa29d8c7aa37886e545aa2a7ebb6f69d842
Gad Nadjar <gadnadjar@gmail.com>
2025-02-05 16:43:41 +0200

    add all function for user, comment and post backend

--- backend/controllers/CommentController.js
+++ backend/controllers/CommentController.js
@@ -1,6 +1,50 @@
-import Comment from '../models/Comment/js'
+import Comment from '../models/Comment.js'
 
 //Create a comment
-export const createComment = (req, res) => {
-    
+export const createComment = async (req, res) => {
+    try {
+        const { comment, author, post_id, user_id } = req.body
+        if (!comment) {
+            res.status(401).json({ message: 'Add a comment' })
+        }
+        const newComment = new Comment({ comment, author, post_id, user_id })
+        const savedComment = await newComment.save()
+        return res.status(200).json(savedComment)
+
+    } catch (error) {
+        return res.status(500).json(error.message)
+    }
+}
+
+//Update a comment
+export const updateComment = async (req, res) => {
+    try {
+        const id = req.params.id
+        const updatedComment = await Comment.findByIdAndUpdate(id, { $set: req.body }, { new: true })
+        return res.status(200).json(updatedComment)
+    } catch (error) {
+        return res.status(500).json(error.message)
+    }
+}
+
+//Delete a comment
+export const deleteComment = async (req, res) => {
+    try {
+        const id = req.params.id
+        await Comment.findByIdAndDelete(id)
+        return res.status(200).json({ message: 'comment deleted successfully' })
+    } catch (error) {
+        return res.status(500).json(error.message)
+    }
+}
+
+//Get all comments
+export const getComments = async (req, res) => {
+    try {
+        const post_id = req.params.id
+        const comments = await Comment.find(post_id)
+        return res.status(200).json(comments)
+    } catch (error) {
+        return res.status(500).json(error.message)
+    }
 }
--- backend/routes/comments.route.js
+++ backend/routes/comments.route.js
@@ -1,8 +1,11 @@
 import express from 'express'
-import { createComment } from '../controllers/CommentController.js'
+import { createComment, deleteComment, getComments, updateComment } from '../controllers/CommentController.js'
 
 const router = express.Router()
 
 router.post('/write', createComment)
+router.put('/:id', updateComment)
+router.delete('/:id', deleteComment)
+router.get('/post/:postId', getComments)
 
 export default router
