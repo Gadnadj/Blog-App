6fde291db826a46cf7238b296902e5485fcedb77
Gad Nadjar <gadnadjar@gmail.com>
2025-02-05 17:45:11 +0200

    add mobile navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
 import { BiSearch } from "react-icons/bi";
+import { FaBars } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import MenuMobile from "./MenuMobile";
 
 const Navbar = () => {
+    const [menu, setMenu] = useState(false);
     const user = false;
+
+    const showMenu = () => {
+        setMenu(true);
+    };
 
     return (
         <div className="flex flex-wrap items-center justify-between w-full max-w-screen-xl mx-auto px-4 py-3 md:px-10">
@@ -12,7 +20,7 @@ const Navbar = () => {
             </h1>
 
             {/* Barre de recherche */}
-            <div className="flex items-center gap-2 border rounded-full px-3 py-1 max-w-[120px] md:max-w-[250px] flex-1">
+            <div className="flex items-center gap-2 border rounded-full px-3 py-1 max-w-[150px] md:max-w-[250px] flex-1">
                 <BiSearch className="text-gray-500" />
                 <input
                     className="w-full outline-none text-sm bg-transparent placeholder-gray-400"
@@ -22,7 +30,7 @@ const Navbar = () => {
             </div>
 
             {/* Liens utilisateur */}
-            <div className="flex items-center gap-3 md:gap-6">
+            <div className="hidden md:flex items-center justify-center gap-2 md:gap-4">
                 {user ? (
                     <>
                         <Link className="text-sm md:text-lg" to="/write">Write</Link>
@@ -34,6 +42,17 @@ const Navbar = () => {
                         <Link className="text-sm md:text-lg" to="/register">Register</Link>
                     </>
                 )}
+            </div>
+
+            <div
+                onClick={showMenu}
+                className="text-lg relative">
+                <p>
+                    <FaBars className="cursor-pointer"/>
+                </p>
+                {
+                    menu && <MenuMobile setMenu={setMenu} />
+                }
             </div>
         </div>
     );
