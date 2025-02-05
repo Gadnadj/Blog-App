2bc4391cdad151c043a486a47329f6ff25ecc535
Gad Nadjar <gadnadjar@gmail.com>
2025-02-05 17:01:22 +0200

    add verify token middleware

---
+++ backend/middleware/verifyToken.js
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken'
+
+export const verifyToken = (req, res, next) => {
+    const token = req.cookies.token
+    if (!token) {
+        return res.status(401).json({ message: 'You are not authenticated' })
+    }
+    jwt.verify(token, process.env.JWT_SECRET, async (err, data) => {
+        if (err) {
+            return res.status(403).json({ message: 'Token is not valid!' })
+        }
+        res.userId = data.id
+        next()
+    })
+}
